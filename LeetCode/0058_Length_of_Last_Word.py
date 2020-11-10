4445dca26775b534a7d3e0ea832e293e454394b6
unknown <wjyeap123@gmail.com>
2020-11-10 17:35:05 +0800

    Added the solution for #0058 - Length of Last Word in Python

--- LeetCode/0058_Length_of_Last_Word.py
+++ LeetCode/0058_Length_of_Last_Word.py
@@ -1,6 +1,7 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
         try:
+            #split the string by space and get the length of last item in the array
             return len(s.split()[-1])
-        except IndexError:
+        except IndexError: #handle empty string
             return 0
